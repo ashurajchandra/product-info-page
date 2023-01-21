1b5ea1ecd41e0457a46ff526fdd86d8f3135aa8a
Ashutosh Rai <ashutoshrai@Ashutoshs-MacBook-Air.local>
2023-01-21 13:26:18 +0530

    added navbar

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log("hello")
+
+
+// const button = document.querySelector("button")
+
+
+// function hnadleClickHandler(){
+//     console.log("hiiiiiii")
+// }
+
+
+// button.addEventListener("click", hnadleClickHandler )
